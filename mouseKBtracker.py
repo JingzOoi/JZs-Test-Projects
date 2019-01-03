590c6b6a37e96fee28a329b406f97e747a98e335
JZ <OoiJingZhi@gmail.com>
2019-01-03 16:45:02 +0800

    added keyboard tracking function

---
+++ mouseKBtracker.py
@@ -0,0 +1,34 @@
+#! python3
+
+import PySimpleGUI as sg
+import pyautogui
+
+layoutMouse = sg.Frame('Mouse Location', [
+    [sg.Text('', size=(10, 1), font='Helvetica 20', justification='center', key='_MousePositionX_')],
+    [sg.Text('', size=(10, 1), font='Helvetica 20', justification='center', key='_MousePositionY_')]
+])
+layoutKB = sg.Frame('Keyboard History', [
+    [sg.Text('Last key pressed:', size=(15,1), font='Helvetica 20', justification='center')],
+    [sg.Text('', font='Helvetica 20', size=(15,1), justification='center', key='_KBHistory_')]
+])
+
+layout = [
+    [layoutMouse, layoutKB],
+    [sg.Button('Move to Center', key = '_btnCenter_', size=(15,3)), sg.Exit(size=(15,3))]
+]
+
+window = sg.Window('MouseNow.py', return_keyboard_events=True, use_default_focus=False).Layout(layout)
+x1, y1 = pyautogui.size()
+
+while True:
+    event, values = window.ReadNonBlocking()
+
+    if event == '_btnCenter_':
+        pyautogui.moveTo(x1/2, y1/2)
+    elif event == 'Exit' or values is None:
+        break
+
+    x, y = pyautogui.position()
+    MouseUpdateX = window.FindElement('_MousePositionX_').Update('X: {}'.format(str(x).rjust(4)))
+    MouseUpdateY = window.FindElement('_MousePositionY_').Update('Y: {}'.format(str(y).rjust(4)))
+    KBUpdate = window.FindElement('_KBHistory_').Update(event)
