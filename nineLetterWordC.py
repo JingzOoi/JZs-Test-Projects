e37bb45d9fd5ba2d2f0af16195544d56da8c0bff
JZ <OoiJingZhi@gmail.com>
2019-01-05 19:43:45 +0800

    added try/except, has addtional notes

--- nineLetterWordC.py
+++ nineLetterWordC.py
@@ -2,6 +2,7 @@
 # nineLetterWordC.py: Cheat even in this easy game: https://nineletterword.tompaton.com/
 # Usage: [1] Launch the script: >>> python nineLetterWordC.py <yourHand>
 # Requirements: requests, bs4, selenium, geckodriver
+# Note: if it saya 'Already guessed', it's normal. Another word with the same combination has already been guessed.
 
 import requests, sys
 from bs4 import BeautifulSoup
@@ -15,31 +16,35 @@ else:
     print('Invalid Hand! Make sure the Hand contains exactly nine (9) letters.')
     exit()
 
-page = requests.get(url)
-page.raise_for_status()
+try:
+    page = requests.get(url)
+    page.raise_for_status()
 
-soup = BeautifulSoup(page.text, 'lxml')
-script = soup.select('script')[1].text.split('[')[2].split(']')[0]
-answer = script.split(',')
+    soup = BeautifulSoup(page.text, 'lxml')
+    script = soup.select('script')[1].text.split('[')[2].split(']')[0]
+    answer = script.split(',')
 
-answerList = []
+    answerList = []
 
-for n in answer:
-    out = n.strip().strip('"')
-    answerList.append(out)
+    for n in answer:
+        out = n.strip().strip('"')
+        answerList.append(out)
 
-print('There are% d possible answers.\nLaunching browser...' % len(answerList))
+    print('There are% d possible answers.\nLaunching browser...' % len(answerList))
 
-driver = webdriver.Firefox()
-driver.get(url)
+    driver = webdriver.Firefox()
+    driver.get(url)
 
-guess = driver.find_element_by_class_name('guess')
-btnGuess = driver.find_element_by_class_name('move')
+    guess = driver.find_element_by_class_name('guess')
+    btnGuess = driver.find_element_by_class_name('move')
 
-for i in range(len(answerList)):
-    guess.send_keys(answerList[i])
-    btnGuess.click()
-    print('[{}] Trying '.format(i+1) + answerList[i] + '...')
+    for i in range(len(answerList)):
+        guess.send_keys(answerList[i])
+        btnGuess.click()
+        print('[{}] Trying '.format(i+1) + answerList[i] + '...')
 
-validList = driver.find_elements_by_class_name('right')
-print('Completed. \nOut of {} possible answers, {} are valid.\nYou can close the browser now.'.format(len(answerList), len(validList)))
+    validList = driver.find_elements_by_class_name('right')
+    print('Completed. \nOut of {} possible answers, {} are valid.\nYou can close the browser now.'.format(len(answerList), len(validList)))
+
+except:
+    print('An error has occured. Check if:\n[1] the device is connected to the Internet, or\n[2] The requirements are installed correctly.')
