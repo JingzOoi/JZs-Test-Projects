e5bda44518f670cee2468b9932dcc64346da15ee
JZ-Linux <ooijingzhi@gmail.com>
2019-01-05 21:52:20 +0800

    removed selenium to work faster on my netbook

--- nineLetterWordC.py
+++ nineLetterWordC.py
@@ -1,14 +1,17 @@
 #! python3
 # nineLetterWordC.py: Cheat even in this easy game: https://nineletterword.tompaton.com/
 # Usage: [1] Launch the script: >>> python nineLetterWordC.py <yourHand>
-# Requirements: requests, bs4, selenium, geckodriver
-# Note: if it saya 'Already guessed', it's normal. Another word with the same combination has already been guessed.
+# Requirements: requests, bs4
+# Note: if it says 'Already guessed', it's normal. Another word with the same combination has already been guessed.
 
 import requests, sys
 from bs4 import BeautifulSoup
-from selenium import webdriver
 
-url = 'https://nineletterword.tompaton.com/' + sys.argv[1]
+try:
+    url = 'https://nineletterword.tompaton.com/' + sys.argv[1].upper()
+except IndexError:
+    print('Missing Argument! Make sure you pass your Hand as an argument.')
+    exit()
 
 if len(sys.argv[1]) == 9:
     print('Calculating...')
@@ -25,26 +28,14 @@ try:
     answer = script.split(',')
 
     answerList = []
-
+    i = 0
     for n in answer:
         out = n.strip().strip('"')
+	i += 1
+	print('[{}] '.format(i) + out)
         answerList.append(out)
 
-    print('There are% d possible answers.\nLaunching browser...' % len(answerList))
-
-    driver = webdriver.Firefox()
-    driver.get(url)
-
-    guess = driver.find_element_by_class_name('guess')
-    btnGuess = driver.find_element_by_class_name('move')
-
-    for i in range(len(answerList)):
-        guess.send_keys(answerList[i])
-        btnGuess.click()
-        print('[{}] Trying '.format(i+1) + answerList[i] + '...')
-
-    validList = driver.find_elements_by_class_name('right')
-    print('Completed. \nOut of {} possible answers, {} are valid.\nYou can close the browser now.'.format(len(answerList), len(validList)))
+    print('There are% d possible answers.' % len(answerList))
 
 except:
     print('An error has occured. Check if:\n[1] the device is connected to the Internet, or\n[2] The requirements are installed correctly.')
