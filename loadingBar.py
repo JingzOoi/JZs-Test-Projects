9c1f231afda14cc1bdb33b3b8cad73d2cfcb17ba
JZ <OoiJingZhi@gmail.com>
2019-02-03 16:48:59 +0800

    small adjustments

--- loadingBar.py
+++ loadingBar.py
@@ -1,5 +1,5 @@
 def loadingBar(maximumNumber, currentNumber, message=''):
     p = (currentNumber/maximumNumber)*100
-    load = '[{}] {}% {}'.format(('█'*(round(p/100*20))).ljust(20, '.'), str(round(p, 2)).ljust(5), message)
+    load = '[{}] {}% | {}'.format(('█'*(round(p/100*20))).ljust(20, '.'), str(round(p, 2)).ljust(6), message)
     print('\b'*len(load), end='', flush=True)
     print(load, end='')
